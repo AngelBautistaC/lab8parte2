45d76222367c31af8818f7469cc9f1999415269b
Angel Alvaro Bautista Cutipa <a20196044@pucp.edu.pe>
2022-11-08 21:57:38 -0500

    final

--- src/main/java/com/example/lab8parte2/dao/CoinsDao.java
+++ src/main/java/com/example/lab8parte2/dao/CoinsDao.java
@@ -2,6 +2,9 @@ package com.example.lab8parte2.dao;
 
 
 import com.example.lab8parte2.entity.coins;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Component;
 
 import org.springframework.http.ResponseEntity;
@@ -14,10 +17,15 @@ import java.util.List;
 @Component
 public class CoinsDao {
 
+
+
+    @JsonProperty("coins")
     public List<coins> listarCategorias() {
+
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<coins[]> response = restTemplate.getForEntity(
-                "https://api.coinstats.app/public/v1/coins?skip=0&limit=10", coins[].class);
+
+        ResponseEntity<coins> response = restTemplate.getForEntity(
+                "https://api.coinstats.app/public/v1/coins?skip=0&limit=10", coins.class);
 
         return Arrays.asList(response.getBody());
     }
--- src/main/java/com/example/lab8parte2/entity/coins.java
+++ src/main/java/com/example/lab8parte2/entity/coins.java
@@ -4,9 +4,11 @@ package com.example.lab8parte2.entity;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
-public class coins {
+public class coins implements Serializable {
     private String id;
     private String symbol;
     private String price;
