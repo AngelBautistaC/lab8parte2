aa8c71d2c6e46de7b06b977b697e43784cb4bb3a
Angel Alvaro Bautista Cutipa <a20196044@pucp.edu.pe>
2022-11-08 20:52:21 -0500

    first

---
+++ src/main/java/com/example/lab8parte2/Lab8parte2Application.java
@@ -0,0 +1,13 @@
+package com.example.lab8parte2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab8parte2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab8parte2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab8parte2/Lab8parte2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab8parte2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab8parte2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
